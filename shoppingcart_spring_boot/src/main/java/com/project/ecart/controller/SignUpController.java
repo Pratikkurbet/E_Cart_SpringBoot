f8ac916951e9cf383e8ea39866ae6f6393be9739
Pratik Venkatesh Kurbet <pratik.kurbet@Yash.com>
2022-11-28 10:38:37 +0530

    packaging convention changed

--- shoppingcart_spring_boot/src/main/java/com/project/ecart/controller/SignUpController.java
+++ shoppingcart_spring_boot/src/main/java/com/project/ecart/controller/SignUpController.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +19,35 @@ import com.project.ecart.service.UserServices.UserService;
 @RequestMapping("api/signup/")
 public class SignUpController {
 	@Autowired
-	  UserService userservice;
+	UserService userservice;
+	
 	@RequestMapping("user")
 	public ResponseEntity<?> userLogin(@RequestBody HashMap<String,String> signupRequest) {
+		System.out.println(signupRequest);
 		try {
-			
 			User user = userservice.signUpUser(signupRequest);
 			return  ResponseEntity.ok(user);
-		}catch(Exception e ) {
-			return ResponseEntity.badRequest().body(new ApiResponse(e.getMessage(), ""));
+		}catch(Exception e) {
+			return ResponseEntity.badRequest().body(new ApiResponse(e.getMessage(),"Try with different mobile Number"));
 		}
+	}
+	
+	@GetMapping("getUser/{id}")
+	public ResponseEntity<?> getUser(@PathVariable("id") Long id)
+	{
+		User user;
+		try {
+			user = userservice.getUserDetailById(id);
+			return ResponseEntity.ok(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.badRequest().body(new ApiResponse(e.getMessage(),"Please check ID that you passed."));
+		}
+	}
+	
+	@PatchMapping("updateUser")
+	public ResponseEntity<?> updateUser()
+	{
+		return null;
 	}
 }
--- shoppingcart_spring_boot/src/main/java/com/project/ecart/model/User.java
+++ shoppingcart_spring_boot/src/main/java/com/project/ecart/model/User.java
@@ -12,7 +12,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	long id;
-	String name, email, password, created_at, login_token, type, address, is_email_verified, mobile;
+	String name, email, password, mobile;
 
 	public long getId() {
 		return id;
@@ -46,51 +46,11 @@ public class User {
 		this.password = password;
 	}
 
-	public String getCreated_at() {
-		return created_at;
-	}
-
-	public void setCreated_at(String created_at) {
-		this.created_at = created_at;
-	}
-
-	public String getLogin_token() {
-		return login_token;
-	}
-
-	public void setLogin_token(String login_token) {
-		this.login_token = login_token;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
 	public String getMobile() {
 		return mobile;
 	}
 
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
-	}
-
-	public String getIs_email_verified() {
-		return is_email_verified;
-	}
-
-	public void setIs_email_verified(String is_email_verified) {
-		this.is_email_verified = is_email_verified;
 	}
 }
