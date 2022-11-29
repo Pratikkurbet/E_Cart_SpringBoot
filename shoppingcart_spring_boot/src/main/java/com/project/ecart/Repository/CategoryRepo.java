d4b71e09dbb2bb2564fb068d9ca5b6b784a55b5f
Pratikkurbet <pratikkurbet02@gmail.com>
2022-11-29 12:22:31 +0530

    Delete shoppingcart_spring_boot directory

--- shoppingcart_spring_boot/src/main/java/com/project/ecart/Repository/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.ecart.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.ecart.model.Category;
-
-public interface CategoryRepo  extends JpaRepository<Category, Long> {
-
-}
